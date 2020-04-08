2ed6215cdaca60e7a2e2b2b9dd5b243f1824327e
André Mendes Marcondes <andremendesmarcondes@gmail.com>
2020-04-07 22:42:42 -0300

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using Strategy;
+using Strategy.Strategy;
+using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+
+            Console.WriteLine("Client: Strategy is set to normal sorting.");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+        }
+    }
+}
---
+++ Strategy/Strategy/ConcreteStrategyB.cs
@@ -0,0 +1,18 @@
+﻿using Strategy.Interface;
+using System.Collections.Generic;
+
+namespace Strategy.Strategy
+{
+    public class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+
+}
